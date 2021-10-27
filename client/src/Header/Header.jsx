bb9d522b471171a78a72e50b7d10dfb5d7fe1bda
Muslim <tsokaev93@mail.ru>
2021-10-27 22:58:34 +0300

    init: start client side of the project

---
+++ client/src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import c from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={c.header}>
+      <h2>Header</h2>
+    </div>
+  );
+};
+
+export default Header;
